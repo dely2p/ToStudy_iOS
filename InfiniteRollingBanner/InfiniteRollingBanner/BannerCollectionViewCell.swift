2fba1f303a2765b9ea8c8adc5d0265d90fa8a696
dely2p <lhyal2p@gmail.com>
2021-01-11 23:58:38 +0900

    스토리보드 UI 구성 및 ViewController 연결

---
+++ InfiniteRollingBanner/InfiniteRollingBanner/BannerCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BannerCollectionViewCell.swift
+//  InfiniteRollingBanner
+//
+//  Created by dely on 2021/01/11.
+//
+
+import UIKit
+
+class BannerCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var bannerImageView: UIImageView!
+}
--- InfiniteRollingBanner/InfiniteRollingBanner/ViewController.swift
+++ InfiniteRollingBanner/InfiniteRollingBanner/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bannerCollectionView: UICollectionView!
+    private let cellIdentifier: String = "banner_collectionview_cell"
+    private let listOfBanner: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        listOfBanner.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = bannerCollectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? BannerCollectionViewCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        return CGSize(width: width, height: width*15/8)
+    }
+}

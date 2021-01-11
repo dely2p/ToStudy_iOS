04dd45f8bd21f461820fcd02b88215ef9b9d03d9
dely2p <lhyal2p@gmail.com>
2021-01-12 00:06:39 +0900

    배너 이미지 추가 및 사이즈 조정, 페이징 기능 추가

--- InfiniteRollingBanner/InfiniteRollingBanner/ViewController.swift
+++ InfiniteRollingBanner/InfiniteRollingBanner/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var bannerCollectionView: UICollectionView!
     private let cellIdentifier: String = "banner_collectionview_cell"
-    private let listOfBanner: [String] = []
+    private let listOfBanner: [String] = ["banner1", "banner2", "banner3", "banner4", "banner5"]
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -24,11 +24,12 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = bannerCollectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as? BannerCollectionViewCell else { return UICollectionViewCell() }
+        cell.bannerImageView.image = UIImage(named: listOfBanner[indexPath.row])
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = UIScreen.main.bounds.width
-        return CGSize(width: width, height: width*15/8)
+        return CGSize(width: width, height: width)
     }
 }

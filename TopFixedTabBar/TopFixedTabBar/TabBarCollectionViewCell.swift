c9ac78967b9a27421f2e1a7648865e173f601468
dely2p <lhyal2p@gmail.com>
2021-01-31 02:29:52 +0900

    top tabbar를 위한 CollectionViewCell 생성

---
+++ TopFixedTabBar/TopFixedTabBar/TabBarCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarCollectionViewCell.swift
+//  TopFixedTabBar
+//
+//  Created by dely on 2021/01/31.
+//
+
+import UIKit
+
+class TabBarCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+}
--- TopFixedTabBar/TopFixedTabBar/ViewController.swift
+++ TopFixedTabBar/TopFixedTabBar/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tabBarCollectionView: UICollectionView!
+    private let listOfContents: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +19,18 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return listOfContents.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = tabBarCollectionView.dequeueReusableCell(withReuseIdentifier: "tabBarCell", for: indexPath) as?TabBarCollectionViewCell else { return UICollectionViewCell() }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        return CGSize(width: width, height: collectionView.frame.height)
+    }
+}

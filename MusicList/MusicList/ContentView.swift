82772433c4008035921dd97b3041bd24bf279627
dely2p <lhyal2p@gmail.com>
2021-02-16 00:54:25 +0900

    코드 추출 및 하단에 Music List 갯수 출력되도록 구현

--- MusicList/MusicList/ContentView.swift
+++ MusicList/MusicList/ContentView.swift
@@ -11,18 +11,16 @@ struct ContentView: View {
     var listOfMusic: [Music] = []
     var body: some View {
         NavigationView {
-            List(listOfMusic) { music in
-                Image(music.thumbnailName)
-                    .resizable()
-                    .frame(width: 30, height: 30)
-                    .cornerRadius(8)
-                    
+            List {
+                ForEach(listOfMusic) { music in
+                    MusicCell(music: music)
+                }
                 
-                VStack(alignment: .leading) {
-                    Text(music.name)
-                    Text("play count: \(music.playCount)")
-                        .font(.subheadline)
+                HStack {
+                    Spacer()
+                    Text("\(listOfMusic.count) Album List")
                         .foregroundColor(.secondary)
+                    Spacer()
                 }
             }
             .navigationTitle("2ND DESIRE[TASTY]")
@@ -33,5 +31,24 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(listOfMusic: data)
+    }
+}
+
+struct MusicCell: View {
+    var music: Music
+    var body: some View {
+        NavigationLink(destination: Text(music.name)) {
+            Image(music.thumbnailName)
+                .resizable()
+                .frame(width: 30, height: 30)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading) {
+                Text(music.name)
+                Text("play count: \(music.playCount)")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }

2b4aa7cc1c814e623728500b25fb26419694f9e7
dely2p <lhyal2p@gmail.com>
2021-02-15 23:22:49 +0900

    swiftUI 리스트 생성

--- MusicList/MusicList/ContentView.swift
+++ MusicList/MusicList/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(0 ..< 5) { item in
+            Image(systemName: "photo")
+            VStack(alignment: .leading) {
+                Text("My MusicList")
+                Text("play count")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
     }
 }
 
